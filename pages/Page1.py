76914a575a2a40755e3056d1c37364c659cfad0c
Fluckigerandreas <andreas.norway@hotmail.com>
2025-09-19 09:30:56 +0000

    Show data

--- pages/Page1.py
+++ pages/Page1.py
@@ -1,5 +1,10 @@
 import streamlit as st
-import pandas as pd 
+import pandas as pd
 
+# Load CSV
 data = pd.read_csv("/workspaces/blank-app/open-meteo-subset.csv")
-print(data)
+
+# Display data in the Streamlit app
+st.title("Weather Data")
+st.subheader("Imported Data Table")
+st.dataframe(data)  # interactive table
