19104a4a05ffa68f2f16ba6e5bb57876feb34c26
Fluckigerandreas <andreas.norway@hotmail.com>
2025-09-19 08:43:15 +0000

    Added to streamlit_app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,36 @@
 import streamlit as st
+import random 
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Collatz conjecture")
+
+#Initialize sessions state 
+if "value" not in st.session_state: 
+    st.session_state.value = None
+    st.session_state.started = False
+    button_label = "Start"
+
+else: 
+    if st.session_state.value == 1:
+        button_label = "Success!"
+    else: 
+        if st.session_state.value % 2 == 0: 
+            button_label = "Half it"
+        else: 
+            button_label = "Triple and add one"
+
+st.button(button_label, disabled=st.session_state.value is 1)
+
+if not st.session_state.started: 
+    st.write("Ready")
+else: 
+    st.write(st.session_state.value)
+
+
+if not st.session_state.started:
+    st.session_state.value = random.randint(1, 100)
+    st.session_state.started = True
+else: 
+    if st.session_state.value % 2 == 0: 
+        st.session_state.value //= 2
+    else: 
+        st.session_state.value = st.session_state.value * 3 + 1
