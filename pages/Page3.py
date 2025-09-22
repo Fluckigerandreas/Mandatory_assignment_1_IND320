e1990be19e16851042f646e10866c7ef0bcacc30
Fluckigerandreas <andreas.norway@hotmail.com>
2025-09-22 08:20:43 +0000

    Meme

--- pages/Page3.py
+++ pages/Page3.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.image(
-    "https://www.google.com/url?sa=i&url=https%3A%2F%2Ftenor.com%2Fsearch%2Fbatman-memes-gifs&psig=AOvVaw0UOCGGHymqwbGLxKv9LwI4&ust=1758615409183000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCLDI8oz3648DFQAAAAAdAAAAABAE", 
+    "https://i.imgflip.com/4/46e43q.jpg", 
     caption="Batman slaps Robin – 'Plot your data properly!'",
     use_container_width=True
 )
