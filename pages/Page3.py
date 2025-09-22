3e611d8098e958d2ee1af0e6e1e66f6d3d5568ac
Fluckigerandreas <andreas.norway@hotmail.com>
2025-09-22 08:19:39 +0000

    URL test

--- pages/Page3.py
+++ pages/Page3.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.image(
-    "https://i.imgflip.com/4/46e43q.jpg", 
+    "https://i.pinimg.com/originals/4b/2a/1b/4b2a1b0a1b0a1b0a1b0a1b0a1b0a1b0a.jpg", 
     caption="Batman slaps Robin – 'Plot your data properly!'",
     use_container_width=True
 )
