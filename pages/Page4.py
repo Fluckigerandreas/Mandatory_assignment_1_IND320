29f32df508d1f6a30669745310579bf11fba0ccd
Fluckigerandreas <andreas.norway@hotmail.com>
2025-10-20 12:24:34 +0000

    Changed path

--- pages/Page4.py
+++ pages/Page4.py
@@ -1,5 +1,5 @@
 import streamlit as st
 from pathlib import Path
 
-image_path = Path(__file__).parent / "a9my9i.jpg"
+image_path = Path(__file__).parent.parent / "a9my9i.jpg"
 st.image(str(image_path), caption="", use_container_width=True)
