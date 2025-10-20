61ad777ac5823539c94c3e3b754a1ccfa46f1898
Fluckigerandreas <andreas.norway@hotmail.com>
2025-10-20 12:29:47 +0000

    Added changes

--- pages/Page4.py
+++ pages/Page4.py
@@ -1,5 +1,8 @@
 import streamlit as st
 from pathlib import Path
+from PIL import Image
 
 image_path = Path(__file__).parent.parent / "a9my9i.jpg"
-st.image(str(image_path), caption="", use_container_width=True)
+img = Image.open(image_path)
+
+st.image(img, caption="Overview of how project 2 has gone", use_container_width=True)
